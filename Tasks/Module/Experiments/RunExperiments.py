fcc70ef320dc752c034bf0b4d2004ed392a3cde1
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-11-11 15:46:54 +0000

    Added docstring and examples to abstract class

--- Tasks/Module/Experiments/RunExperiments.py
+++ Tasks/Module/Experiments/RunExperiments.py
@@ -2,8 +2,8 @@ from sklearn.model_selection import KFold
 from abc import ABC, abstractmethod
 from . import GetData as gd
 
-def run_(classifier):
-    X = gd.get_data(1)
+def run_experiments(classifier):
+    X, y = gd.get_data(1)
 
     kf = KFold(n_splits=10)
     for train, test in kf.split(X):
@@ -13,7 +13,20 @@ def run_(classifier):
 class Classifier(ABC):
     @abstractmethod
     def run_classifier(self, X, y):
-        pass
+        """Abstract class to use for running experiments
+
+        :param X: X data
+        :type X: pandas.dataframe
+        :param y: y labels for data
+        :type y: pandas.dataframe
+        :return: 4-tuple of confusion matrix
+        :rtype: (int, int, int, int)
+        """
+        true_positive = None
+        false_positive = None
+        false_negative = None
+        true_negative = None
+        return (true_positive, false_positive, false_negative, true_negative)
 
 
 # Example of using inheritence
@@ -24,5 +37,8 @@ class NaiveBayse(Classifier):
 
     def run_classifier(self, X, y):
         self.classifier.fit(X, y)
+        # do classifier stuff in here
+        return self.score()
 
-        return 'scores'
+    def score(self):
+        return (0,0,0,0)
