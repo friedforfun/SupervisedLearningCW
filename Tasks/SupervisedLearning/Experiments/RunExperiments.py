cf46b2e542c11145d9efa707fa9bf418ea25b09f
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-11-14 15:07:53 +0000

    Run an experiment should be done

--- Tasks/SupervisedLearning/Experiments/RunExperiments.py
+++ Tasks/SupervisedLearning/Experiments/RunExperiments.py
@@ -1,7 +1,9 @@
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.metrics import confusion_matrix
 from abc import ABC, abstractmethod
 from . import GetData as gd
+
+
 
 def run_experiments(classifier):
     # These experiments for each classifier:
@@ -10,12 +12,31 @@ def run_experiments(classifier):
     # a new  set with 9000 instances of the training set moved into the test set
     raise NotImplementedError
 
-def run_experiment(classifier):
-    # Step 1:
+def run_experiment(classifier, X, y, stratified=False, n_splits=10, **kwargs):
     # Run classifiers using 10-fold cross validation for various learning parameters on the training sets
-    # Step 2:
-    # Visualise results
-    raise NotImplementedError
+    X = X.to_numpy()
+    y = y.to_numpy()
+
+    if stratified:
+        # StratifiedKFold(n_splits=5, shuffle=False, random_state=None)
+        kf = StratifiedKFold(n_splits=n_splits, **kwargs)
+    else:
+        # KFold(n_splits=5, shuffle=False, random_state=None)
+        kf = KFold(n_splits=n_splits, **kwargs)
+
+    train_scores = {}
+    test_scores = {}
+    for i, (train_indices, test_indices) in enumerate(kf.n_splits(X)):
+        X_train, X_test = X[train_indices], X[test_indices]
+        y_train, y_test = y[train_indices], y[test_indices]
+
+        train_scores[i] = classifier.run_classifier(X_train, y_train)
+        test_scores[i] = classifier.run_classifier(X_test, y_test)
+    
+    return train_scores, test_scores
+
+# Visulisation
+# https://scikit-learn.org/stable/auto_examples/model_selection/plot_cv_indices.html#sphx-glr-auto-examples-model-selection-plot-cv-indices-py
 
 def k_folds(classifier):
     X, y = gd.get_data(1)
@@ -49,9 +70,9 @@ class Classifier(ABC):
         """Abstract class to use for running experiments
 
         :param X: X data
-        :type X: pandas.dataframe
+        :type X: numpy.array
         :param y: y labels for data
-        :type y: pandas.dataframe
+        :type y: numpy.array
         :return: An instance of a confusion matrix from sklearn
         :rtype: confusion_matrix
         """
@@ -65,6 +86,7 @@ class NaiveBayse(Classifier):
     def __init__(self, typ='gaussian'):
         if typ == 'gaussian':
             self.classifier = None
+        
 
     def run_classifier(self, X, y):
         self.classifier.fit(X, y)
