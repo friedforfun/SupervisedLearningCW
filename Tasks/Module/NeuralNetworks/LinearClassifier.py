df7d01d823c81251da37ddde2155cec4cf1d1b87
Kamil <kamil_szymczak@hotmail.com>
2020-11-14 13:44:00 +0000

    LinearClassifier WIP

--- Tasks/Module/NeuralNetworks/LinearClassifier.py
+++ Tasks/Module/NeuralNetworks/LinearClassifier.py
@@ -1,7 +1,21 @@
 from ..Experiments import RunExperiments as re
+from sklearn.linear_model import LogisticRegression
 
 class LinearClassifier(re.Classifier):
+    def __init__():
 
 
     def run_classifier(self, X, y):
-        raise NotImplementedError
+        # lr = LogisticRegression()
+        # lr.fit(X, y)
+
+        self.classifier.fit(X, y)
+
+        y_pred = lr.predict(x_test)
+
+        return lr.score(X, y)
+        return confusion_matrix(y_test, y_pred)
+
+    def score(self):
+        return confusion_matrix(y_test, y_pred)
+
