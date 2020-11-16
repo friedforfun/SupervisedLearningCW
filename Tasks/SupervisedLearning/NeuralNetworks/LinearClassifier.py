8de43efe947fad17393e77defbc4abe654dd2d5f
Kamil <kamil_szymczak@hotmail.com>
2020-11-16 12:59:17 +0000

    LinearClassifier: docstrings added

--- Tasks/SupervisedLearning/NeuralNetworks/LinearClassifier.py
+++ Tasks/SupervisedLearning/NeuralNetworks/LinearClassifier.py
@@ -1,19 +1,42 @@
-from ..Experiments.RunExperiments import Classifier
+from ..Experiments.abstract import Classifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 
 class LinearClassifier(Classifier):
+    """A Linear Classifier with an interface used for running experiments
+    """
     def __init__(self, kind='lr', **kwargs):
         self.lr = LogisticRegression(**kwargs)
 
     def build_classifier(self, X, y):
+        """Fit the classifier to the provided data
+
+        :param X: X data
+        :type X: numpy.array
+        :param y: y labels for data
+        :type y: numpy.array
+        """
         self.lr.fit(X, y)
 
     def run_classifier(self, X, y):
+        """Make a prediction and return the confusion matrix for a given labelled dataset
+
+        :param X: X data
+        :type X: numpy.array
+        :param y: y labels for data
+        :type y: numpy.array
+        :return: An instance of a confusion matrix from sklearn
+        :rtype: confusion_matrix
+        """
         pred = self.lr.predict(X)
         return confusion_matrix(y, pred)
 
     def get_classifier(self):
+        """Get the lr classifier object
+
+        :return: The configured LR object
+        :rtype: sklearn.linear_model.LogisticRegression
+        """
         return self.lr
 
     def prediction(self, x):
