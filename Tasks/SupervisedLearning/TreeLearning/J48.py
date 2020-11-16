a3185c432e794e6dfb191652a5ab2bc2d48d106e
Lewis <Lewiswilson1497@gmail.com>
2020-11-16 12:37:32 +0000

    J48 class

--- Tasks/SupervisedLearning/TreeLearning/J48.py
+++ Tasks/SupervisedLearning/TreeLearning/J48.py
@@ -1,7 +1,25 @@
 from ..Experiments.abstract import Classifier
+from sklearn.metrics import confusion_matrix
+from sklearn import tree as j48
+import graphviz
 
 class J48(Classifier):
+    def __init__(self):
+        self.j48 = j48.DecisionTreeClassifier()
 
+        def build_classifier(self,X,y):
+            self.j48.fit(X,y)
 
-    def run_classifier(self, X, y):
-        raise NotImplementedError
+        def run_classifier(self, X, y):
+            self.j48.predict(X)
+            
+        def build_conf(self, X, y):
+            y_pred = self.j48.predict(X)
+            return confusion_matrix(y, y_pred)
+        
+        def print_tree():
+            dot_data = j48.export_graphviz(self.j48, out_file=None) 
+            graph = graphviz.Source(dot_data) 
+            graph.render("j48_graph") 
+                        
+            
