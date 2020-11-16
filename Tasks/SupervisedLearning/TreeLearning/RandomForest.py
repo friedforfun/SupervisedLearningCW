2d8ceea64e8bfe841b0eb52a1d98db6bf7288f79
jimmym620 <jimmym620@gmail.com>
2020-11-16 13:28:20 +0000

    Added predict and predict proba to RandomForest

--- Tasks/SupervisedLearning/TreeLearning/RandomForest.py
+++ Tasks/SupervisedLearning/TreeLearning/RandomForest.py
@@ -1,19 +1,57 @@
 from ..Experiments.abstract import Classifier
 from sklearn.metrics import confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
+import graphviz
 
 class RandomForest(Classifier):
-    def __init__(self, typ = 'rf'):
-        self.rf = RandomForestClassifier(verbose=3)
-
-    def build_classifier(self,X,y):
-        self.rf.fit(X,y)
+    """A Random Forest with an interface used for running experiments
+    """
+    def __init__(self):
+        self.rf = RandomForestClassifier(verbose=2)
 
     def run_classifier(self, X, y):
-        self.rf.predict(X)
+            """Make a prediction and return the confusion matrix for given labelled dataset
 
-    def build_conf(self, X, y):
+            :param X: X data
+            :type X: numpy.array
+            :param y: y labels for data
+            :type y: numpy.array
+            :return: An instance of a confusion matrix from sklearn
+            :rtype: confusion_matrix
+            """
+            y_pred = self.rf.predict(X)
+            return confusion_matrix(y, y_pred)
+
+    def build_classifier(self,X,y):
+        """Fit the classifier to the provided data
+        
+        :param X: X data
+        :type X: numpy.array
+        :param y: labels for data
+        :type y: numpy array
+        """
+        self.rf.fit(X,y)
+
+    def get_classifier(self):
+        """A method to return the classifier object 
+        """
+        return self.rf
+
+    def prediction(self,X):
+        """Make a prediction given X
+
+        :param X: Data to make prediction on
+        :type X: numpy.array
+        """
         y_pred = self.rf.predict(X)
-        return confusion_matrix(y, y_pred)
+        return y_pred
+
+    def prediction_proba(self,X,y):
+        """Find the probablitities for each prediction
+
+        :param X: Data to make prediction on
+        :type X: numpy.array
+        """
+        return self.rf.predict_proba(X)    
+
     
