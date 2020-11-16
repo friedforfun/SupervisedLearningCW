8e600ee67540efd305d6097fb961d66e0d67e999
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-11-16 11:36:07 +0000

    completed multilayer perceptron

--- Tasks/SupervisedLearning/NeuralNetworks/MultiLayerPerceptron.py
+++ Tasks/SupervisedLearning/NeuralNetworks/MultiLayerPerceptron.py
@@ -1,7 +1,42 @@
 from ..Experiments.abstract import Classifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
 
 class MultiLayerPerceptron(Classifier):
+    """A multilayer perceptron with an interface used for running experiments
+    """
+    def __init__(self, **kwargs):
+        self.mlp = MLPClassifier(**kwargs)
 
+    def get_classifier(self):
+        """Get the mlp classifier object
+
+        :return: The configured MLP object
+        :rtype: sklearn.neural_network.MLPClassifier
+        """
+        return self.mlp
+    
+    def build_classifier(self, X, y):
+        """Fit the classifier to the provided data
+
+        :param X: X data
+        :type X: numpy.array
+        :param y: y labels for data
+        :type y: numpy.array
+        """
+        self.mlp.fit(X, y)
 
     def run_classifier(self, X, y):
-        raise NotImplementedError
+        """Make a prediction and return the confusion matrix for a given labelled dataset
+
+        :param X: X data
+        :type X: numpy.array
+        :param y: y labels for data
+        :type y: numpy.array
+        :return: An instance of a confusion matrix from sklearn
+        :rtype: confusion_matrix
+        """
+        pred = self.mlp.predict(X)
+
+        return confusion_matrix(y, pred)
+
