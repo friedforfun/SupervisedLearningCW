e079d684b068ae15f6a1a0e196d4276a10fd1e0b
Lewis <Lewiswilson1497@gmail.com>
2020-11-21 12:13:57 +0000

    Starting to update docu strings

--- Tasks/SupervisedLearning/TreeLearning/J48.py
+++ Tasks/SupervisedLearning/TreeLearning/J48.py
@@ -4,22 +4,82 @@ from sklearn import tree as j48
 import graphviz
 
 class J48(Classifier):
-    def __init__(self):
-        self.j48 = j48.DecisionTreeClassifier()
+    """ J48 implementation used via an interface for running experiments 
 
-        def build_classifier(self,X,y):
-            self.j48.fit(X,y)
+    :param Classifier: [description]
+    :type Classifier: [type]
+    """
+    
 
-        def run_classifier(self, X, y):
-            self.j48.predict(X)
-            
-        def build_conf(self, X, y):
-            y_pred = self.j48.predict(X)
-            return confusion_matrix(y, y_pred)
+    def __init__(self, **kwargs):
+        self.hyper_params = kwargs
+        self.j48 = j48.DecisionTreeClassifier(**kwargs)
+
+    def build_classifier(self,X,y):
+        """Fit the classifier to the provided data
+
+        :param X: X data
+        :type X: numpy.array
+        :param y: labels for data
+        :type y: numpy.array
+        """
+        self.j48.fit(X,y)
+
+    def run_classifier(self, X, y):
+        """[summary]
+
+        :param X: [description]
+        :type X: [type]
+        :param y: [description]
+        :type y: [type]
+        :return: [description]
+        :rtype: [type]
+        """
         
-        def print_tree():
-            dot_data = j48.export_graphviz(self.j48, out_file=None) 
-            graph = graphviz.Source(dot_data) 
-            graph.render("j48_graph") 
-                        
+        pred = self.j48.predict(X)
+        return confusion_matrix(y, pred)
+            
+    def prediction(self, X):
+        """Make a prediction given X
+
+        :param X:  Data to make prediction on
+        :type X: numpy.array
+        """
+        
+        self.j48.predict(X)
+    
+    def prediction_proba(self, X):
+        """Find the probablitities for each prediction
+
+        :param X: Data to make prediction on
+        :type X: numpy.array
+        """
+        
+        self.j48.predict_proba(X)
+        
+    def get_params(self):
+        """[summary]
+
+        :return: [description]
+        :rtype: [type]
+        """
+        
+        return self.hyper_params
+    
+    def get_classifier(self):
+        """[summary]
+
+        :return: [description]
+        :rtype: [type]
+        """
+        
+        return self.j48
+                            
+    def print_tree():
+        """[summary]
+        """
+        
+        dot_data = j48.export_graphviz(self.j48, out_file=None) 
+        graph = graphviz.Source(dot_data) 
+        graph.render("j48_graph") 
             
