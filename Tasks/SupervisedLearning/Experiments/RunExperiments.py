bb89f771ce2dbe89e42c8d4b09bbf37c079c3865
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-12-01 12:15:24 +0000

    Included accuracy score

--- Tasks/SupervisedLearning/Experiments/RunExperiments.py
+++ Tasks/SupervisedLearning/Experiments/RunExperiments.py
@@ -1,6 +1,7 @@
 import pandas
 from SupervisedLearning.Experiments.GetData import append_result_col, get_data
 from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.metrics import accuracy_score
 import wandb
 from . import GetData as gd
 
@@ -113,7 +114,8 @@ def run_KFold_experiment(classifier, X, y, classifier_name='', classes_desc='all
             test_scores[i] = classifier.run_classifier(X_test, y_test)
             wandb.sklearn.plot_classifier(classifier.get_classifier(), 
                 X_train, X_test, y_train, y_test, y_pred, y_probs, labels=class_labels, model_name=classifier_name)
-            wandb.sklearn.plot_confusion_matrix(y_test, y_pred, class_labels)
+            wandb.log({'Accuracy': accuracy_score(y_test, y_pred)})
+            #wandb.sklearn.plot_confusion_matrix(y_test, y_pred, class_labels)
 
     return train_scores, test_scores
 
